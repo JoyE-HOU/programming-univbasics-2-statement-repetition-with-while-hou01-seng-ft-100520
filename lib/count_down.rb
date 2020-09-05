e920b75c74a9ef72f15a3c80f523349ba37d8fcd
Joy Evans <joyevans@me.com>
2020-09-05 21:17:54 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,6 @@
 magic_exit_number = 0 
 count = 10 
-while count > 10
-break if count == magic_exit_number
+while count > 10 && count != magic_exit_number do 
 puts "Happy New Year"
 count -= 10 
 end
